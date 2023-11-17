1414dbbcc51e4ed798c9a9aa89c19c8fe99b9a58
Matthew Shinkle <mshinkle@nevada.unr.edu>
2023-11-17 09:27:47 -0800

    feat: add utils.py and p-value computation

--- copterr/copterr.py
+++ copterr/copterr.py
@@ -2,119 +2,11 @@ import numpy as np
 import torch
 import scipy
 from tqdm import tqdm
-
-
-def create_permutation_idxs(n_tpts, n_permutations, block_len):
-    """Very similar to utils.get_permutation_index, but varies where it begins slicing,
-    such that the values within each block will vary, not just the block order.
-    Should probably be merged in some way into a single function in the future.
-
-    Parameters
-    ----------
-    n_tpts : int
-        Length of the dimension to be permuted.
-    n_permutations : int
-        Number of different rows of permuted indices to return.
-        Returned array will be n_permutations X n_tpts
-    block_len : int
-        Length of 'blocks' to divide list of indices into prior to
-        permuting these blocks.  Helps maintain temporal correlations that
-        exist regardless of whether meaningful signal exists--correlations that
-        would be destroyed with block-less permutation.
-
-    Returns
-    -------
-    np.ndarray
-        n_permutations X n_tpts array of block-permuted indices.  To loop through permutations,
-        loop through rows.
-    """
-    permutation_idxs = np.empty((n_permutations, n_tpts), dtype=int)
-    n_blocks = np.ceil(n_tpts / block_len)
-    mod = n_tpts % block_len
-
-    for permutation in range(n_permutations):
-        idxs = []
-        mod_block = np.random.randint(0, n_blocks)
-        i = 0
-        block_idxs = []
-        current_mod = 0
-        while True:
-            if i == n_tpts:
-                idxs.append(block_idxs)
-                break
-            if len(idxs) + 1 == mod_block:
-                current_mod = block_len - mod
-            if i > 0 and (i + current_mod) % block_len == 0:
-                idxs.append(block_idxs)
-                block_idxs = []
-            block_idxs.append(i)
-            i += 1
-        np.random.shuffle(idxs)
-        permutation_idxs[permutation] = np.hstack(idxs)
-    return permutation_idxs
-
-
-def column_corr_torch(A, B, unbiased=False):
-    """Similar to Stats.utils.column_corr, except works with tensors.  Should be tested more
-    thoroughly for possible differences.
-
-    Parameters
-    ----------
-    A : np.ndarray
-        First 2d array to measure correlation with other array.
-    B : np.ndarray
-        Other 2d array to measure correlation with other array (order does not matter).
-    unbiased : bool
-        Whether to compute std as a biased estimator.  False computes equivalently to 
-        dof=0 in column_corr.
-
-    Returns
-    -------
-    np.ndarray
-        1d array, with each value representing the correlation between the corresponding
-        columns of arrays A and B.
-    """
-    A, B = torch.tensor(A), torch.tensor(B)
-    def zs(x): 
-        return (x-torch.mean(x, dim=-2, keepdims=True)) / torch.std(x, dim=-2, keepdims=True, unbiased=unbiased)
-    rTmp = torch.sum(zs(A)*zs(B), dim=-2, keepdims=True)
-    n = A.shape[0]
-    # make sure not to count nans
-    nNaN = torch.sum(torch.isnan(zs(A)) | torch.isnan(zs(B)), dim=-2, keepdims=True)
-    n = n - nNaN
-    r = rTmp/n
-    return r.squeeze()
-
-
-def quantize_alphas(alphas, base=10):
-    alphas_quantized = base**np.round(np.emath.logn(base, alphas))
-    return alphas_quantized
-
-
-def alphas_to_deltas(alphas):
-    deltas = 2*np.log(alphas.T)
-    return deltas
-
-
-def deltas_to_alphas(deltas):
-    alphas = np.exp(deltas.T/2)
-    return alphas
-
-
-def _process_perm_idxs(permutation, Y_len, block_len):
-    if isinstance(permutation, np.ndarray):
-        pass
-    elif isinstance(permutation, bool):
-        if permutation is True:
-            permutation = create_permutation_idxs(Y_len, 1, block_len)[0]
-        elif permutation is False:
-            permutation = np.arange(Y_len)    
-    elif isinstance(permutation, (list, tuple)):
-        permutation = np.array(permutation)
-    else:
-        raise ValueError("Input 'permutation' should be either False (no permutation), True (to auto-generate), or either a list, tuple, or numpy array of indices.")
-    return permutation
-
+from .utils import (
+    _process_perm_idxs, 
+    column_corr_torch, 
+    deltas_to_alphas, 
+)
 
 class PermuteWeights():
     def __init__(self, X, Y, alphas, device='cpu', dtype=torch.float):
@@ -147,6 +39,8 @@ class PermuteWeights():
         return self.weights
     
     def score(self, Xtest, Ytest, permutation=False, block_len=1, correlation=False):
+        Xtest = torch.tensor(Xtest, device=self.device, dtype=self.dtype)
+        Ytest = torch.tensor(Ytest, device=self.device, dtype=self.dtype)
         perm_idxs = _process_perm_idxs(permutation, Ytest.shape[-2], block_len)
         Ypred = Xtest@self.weights
         score = column_corr_torch(Ypred, Ytest[...,perm_idxs,:])
@@ -190,6 +84,8 @@ class PermuteWeightsGrouped():
         return self.weights
     
     def score(self, Xtest, Ytest, permutation=False, block_len=1, correlation=False):
+        Xtest = torch.tensor(Xtest, device=self.device, dtype=self.dtype)
+        Ytest = torch.tensor(Ytest, device=self.device, dtype=self.dtype)
         perm_idxs = _process_perm_idxs(permutation, Ytest.shape[-2], block_len)
         Ypred = Xtest@self.weights
         score = column_corr_torch(Ypred, Ytest[...,perm_idxs,:])
---
+++ copterr/utils.py
@@ -0,0 +1,82 @@
+import numpy as np
+import torch
+
+def create_permutation_idxs(n_timepoints, n_permutations, block_len=1):
+    permutation_idxs = []
+    for _ in range(n_permutations):
+        idxs = np.arange(n_timepoints)
+        start_idx = np.random.randint(block_len)+1
+        start_block, remaining_idxs = np.array_split(idxs, [start_idx])
+        blocks = [start_block] + np.array_split(remaining_idxs, np.arange(block_len,len(remaining_idxs),block_len))
+        np.random.shuffle(blocks)
+        permutation_idxs.append(np.concatenate(blocks))
+    return permutation_idxs
+
+
+def column_corr_torch(A, B, unbiased=False):
+    """Similar to Stats.utils.column_corr, except works with tensors.  Should be tested more
+    thoroughly for possible differences.
+
+    Parameters
+    ----------
+    A : np.ndarray
+        First 2d array to measure correlation with other array.
+    B : np.ndarray
+        Other 2d array to measure correlation with other array (order does not matter).
+    unbiased : bool
+        Whether to compute std as a biased estimator.  False computes equivalently to 
+        dof=0 in column_corr.
+
+    Returns
+    -------
+    np.ndarray
+        1d array, with each value representing the correlation between the corresponding
+        columns of arrays A and B.
+    """
+    def zs(x): 
+        return (x-torch.mean(x, dim=-2, keepdims=True)) / torch.std(x, dim=-2, keepdims=True, unbiased=unbiased)
+    rTmp = torch.sum(zs(A)*zs(B), dim=-2, keepdims=True)
+    n = A.shape[0]
+    # make sure not to count nans
+    nNaN = torch.sum(torch.isnan(zs(A)) | torch.isnan(zs(B)), dim=-2, keepdims=True)
+    n = n - nNaN
+    r = rTmp/n
+    return r.squeeze()
+
+
+def quantize_alphas(alphas, base=10):
+    alphas_quantized = base**np.round(np.emath.logn(base, alphas))
+    return alphas_quantized
+
+
+def alphas_to_deltas(alphas):
+    deltas = 2*np.log(alphas.T)
+    return deltas
+
+
+def deltas_to_alphas(deltas):
+    alphas = np.exp(deltas.T/2)
+    return alphas
+
+
+def compute_p_values(true_values, permutation_values, two_tailed=False):
+    if two_tailed:
+        true_values = abs(true_values)
+        permutation_values = abs(permutation_values)
+    p_values = (permutation_values >= true_values).mean(0)
+    return p_values
+
+
+def _process_perm_idxs(permutation, Y_len, block_len):
+    if isinstance(permutation, np.ndarray):
+        pass
+    elif isinstance(permutation, bool):
+        if permutation is True:
+            permutation = create_permutation_idxs(Y_len, 1, block_len)[0]
+        elif permutation is False:
+            permutation = np.arange(Y_len)    
+    elif isinstance(permutation, (list, tuple)):
+        permutation = np.array(permutation)
+    else:
+        raise ValueError("Input 'permutation' should be either False (no permutation), True (to auto-generate), or either a list, tuple, or numpy array of indices.")
+    return permutation
