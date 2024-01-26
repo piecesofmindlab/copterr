d157973d9fc331d40ecad16dbfc37ed2754f9f13
mshinkle <mshinkle@nevada.unr.edu>
2024-01-26 14:17:38 -0800

    feat: change default svd to torch and add himalaya-like scoring

--- copterr/copterr.py
+++ copterr/copterr.py
@@ -1,18 +1,17 @@
 import numpy as np
 import torch
-import scipy
 from tqdm import tqdm
 from .utils import (
     _process_perm_idxs, 
-    column_corr_torch, 
     deltas_to_alphas, 
+    r2_score,
 )
 
 class PermuteWeights():
     def __init__(self, X, Y, alphas, device='cpu', dtype=torch.float):
         self.device = device
         self.dtype = dtype
-        self.X = X
+        self.X = torch.tensor(X, device=self.device, dtype=self.dtype)
         self.Y = torch.tensor(Y, device=self.device, dtype=self.dtype)
         self.alphas = alphas
         self.weights = torch.zeros((self.X.shape[1], self.Y.shape[1]), device=self.device, dtype=self.dtype)
@@ -21,13 +20,13 @@ class PermuteWeights():
         unique_alphas, alphas_idxs = np.unique(self.alphas, axis=0, return_inverse=True)
         self._alpha_masks = [alphas_idxs==i for i in range(len(unique_alphas))]
         self._all_VDUt = []
-        U, s, Vt = scipy.linalg.svd(self.X, full_matrices=False)
+        U, s, Vh = torch.linalg.svd(self.X, full_matrices=False)
         if verbose:
             unique_alphas = tqdm(unique_alphas, desc='Computing Initial SVDs')
         for alpha in unique_alphas:
             d = s/(s**2 + alpha)
-            VDUt = (Vt.T * d) @ U.T
-            self._all_VDUt.append(torch.tensor(VDUt, device=self.device, dtype=self.dtype))
+            VDUt = (Vh.T * d) @ U.T
+            self._all_VDUt.append(VDUt)
         
     def fit_true_weights(self):
         return self.fit_permutation(permutation=False)
@@ -38,16 +37,15 @@ class PermuteWeights():
             self.weights[:,alpha_mask] = VDUt @ self.Y[perm_idxs][:,alpha_mask]
         return self.weights
     
-    def score(self, Xtest, Ytest, permutation=False, block_len=1, correlation=False):
-        Xtest = torch.tensor(Xtest, device=self.device, dtype=self.dtype)
-        Ytest = torch.tensor(Ytest, device=self.device, dtype=self.dtype)
-        perm_idxs = _process_perm_idxs(permutation, Ytest.shape[-2], block_len)
-        Ypred = Xtest@self.weights
-        score = column_corr_torch(Ypred, Ytest[...,perm_idxs,:])
-        if correlation:
-            return score
-        else:
-            return torch.sign(score)*torch.square(torch.abs(score))
+    def predict(self, X):
+        X = torch.tensor(X, dtype=self.dtype, device=self.device)
+        return X@self.weights
+    
+    def score(self, X, Y, permutation=False, block_len=1):
+        Y = torch.tensor(Y, device=self.device, dtype=self.dtype)
+        perm_idxs = _process_perm_idxs(permutation, Y.shape[-2], block_len)
+        Y_pred = self.predict(X)
+        return r2_score(Y[...,perm_idxs,:], Y_pred)
 
 
 class PermuteWeightsGrouped():
@@ -55,7 +53,7 @@ class PermuteWeightsGrouped():
         self.device = device
         self.dtype = dtype
         self.feature_counts = [fs.shape[1] for fs in X]
-        self.X = np.concatenate(X, axis=1)
+        self.X = torch.tensor(np.concatenate(X, axis=1), device=self.device, dtype=self.dtype)
         self.Y = torch.tensor(Y, device=self.device, dtype=self.dtype)
         if deltas is not None:
             self.alphas = deltas_to_alphas(deltas)
@@ -68,11 +66,11 @@ class PermuteWeightsGrouped():
         if verbose:
             unique_alphas = tqdm(unique_alphas, desc='Computing Initial SVDs')
         for alpha in unique_alphas:
-            scaling = np.hstack([np.full(fs_size, a**.5) for fs_size, a in zip(self.feature_counts, alpha)])
-            U, s, Vt = scipy.linalg.svd(self.X/scaling, full_matrices=True)
+            scaling = torch.hstack([torch.full(fs_size, a**.5, device=self.device, dtype=self.dtype) for fs_size, a in zip(self.feature_counts, alpha)])
+            U, s, Vh = torch.linalg.svd(self.X/scaling, full_matrices=True)
             d = s/(s**2 + 1)
-            VDUt = ((1/scaling * Vt).T[:,:len(d)] * d) @ U.T
-            self._all_VDUt.append(torch.tensor(VDUt, device=self.device, dtype=self.dtype))
+            VDUt = ((1/scaling * Vh).T[:,:len(d)] * d) @ U.T
+            self._all_VDUt.append(VDUt)
     
     def fit_true_weights(self):
         return self.fit_permutation(permutation=False)
@@ -83,13 +81,14 @@ class PermuteWeightsGrouped():
             self.weights[:,alpha_mask] = VDUt @ self.Y[perm_idxs][:,alpha_mask]
         return self.weights
     
-    def score(self, Xtest, Ytest, permutation=False, block_len=1, correlation=False):
-        Xtest = torch.tensor(Xtest, device=self.device, dtype=self.dtype)
-        Ytest = torch.tensor(Ytest, device=self.device, dtype=self.dtype)
-        perm_idxs = _process_perm_idxs(permutation, Ytest.shape[-2], block_len)
-        Ypred = Xtest@self.weights
-        score = column_corr_torch(Ypred, Ytest[...,perm_idxs,:])
-        if correlation:
-            return score
-        else:
-            return torch.sign(score)*torch.square(torch.abs(score))
+    def predict(self, X):
+        if not isinstance(X, (list, tuple)):
+            X = [X]
+        X = torch.hstack([torch.tensor(x, dtype=self.dtype, device=self.device) for x in X])
+        return X@self.weights
+    
+    def score(self, X, Y, permutation=False, block_len=1):
+        Y = torch.tensor(Y, device=self.device, dtype=self.dtype)
+        perm_idxs = _process_perm_idxs(permutation, Y.shape[-2], block_len)
+        Y_pred = self.predict(X)
+        return r2_score(Y[...,perm_idxs,:], Y_pred)
